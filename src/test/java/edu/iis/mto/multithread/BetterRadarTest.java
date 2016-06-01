f4da4e262d3005cb22031c4b285f36356c443ed5
Kadłubek <179640@edu.p.lodz.pl>
2016-06-01 22:19:54 +0200

    Empty BetterRadar class and its test created

---
+++ src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.multithread;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BetterRadarTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
