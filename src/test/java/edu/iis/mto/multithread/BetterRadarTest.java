df6b9ffddff2976f2da85980e2a0bc2141aa377c
Kadłubek <179640@edu.p.lodz.pl>
2016-06-01 22:39:13 +0200

    Implemented test for BetterRadar

--- src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -3,6 +3,8 @@ package edu.iis.mto.multithread;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import java.util.concurrent.Executor;
 
 import org.junit.Test;
 
@@ -11,9 +13,18 @@ public class BetterRadarTest {
 	@Test
 	public void launchPatriotOnceWhenNoticesAScudMissle() {
 		PatriotBattery batteryMock = mock(PatriotBattery.class);
-		Radar radar = new Radar(batteryMock);
+		
+		Executor executor = new Executor(){
+			@Override
+			public void execute(Runnable command) {
+				command.run();
+				
+			}
+		};
+		
+		RadarIfc radar = new BetterRadar(batteryMock, executor);
 		radar.notice(new Scud());
-		verify(batteryMock).launchPatriot();
+		verify(batteryMock, times(10)).launchPatriot();
 	}
 
 }
--- src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -9,7 +9,7 @@ public class RadarTest {
 	@Test
 	public void launchPatriotOnceWhenNoticesAScudMissle() {
 		PatriotBattery batteryMock = mock(PatriotBattery.class);
-		Radar radar = new Radar(batteryMock);
+		RadarIfc radar = new Radar(batteryMock);
 		radar.notice(new Scud());
 		verify(batteryMock).launchPatriot();
 	}
